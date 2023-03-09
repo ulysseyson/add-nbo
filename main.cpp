8d7bd3eb855dd717ca68cdced356ad5ae1794986
ulysseyson <ulysseyson57@gmail.com>
2023-03-09 17:32:13 +0900

    add ntohs & ntohl

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ void dump(void* p, size_t n) {
 	uint8_t* u8 = static_cast<uint8_t*>(p);
 	size_t i = 0;
 	while (true) {
-		printf("%02X ", *u8++);
+		printf("%02X ", *u8++);	
 		if (++i >= n) break;
 		if (i % 8 == 0)
 			printf("\n");
@@ -20,14 +20,20 @@ void write_4660() {
 	dump(&port, sizeof(port));
 }
 
-
+uint16_t my_ntohs(uint16_t n) {
+	return n << 8 | n >> 8;
+}
 
 void  write_0x1234() {
 	uint8_t network_buffer[] = { 0x12, 0x34 };
 	uint16_t* p = reinterpret_cast<uint16_t*>(network_buffer);
-	uint16_t n = *p; // TODO
+	uint16_t n = my_ntohs(*p); // TODO
 
 	printf("16 bit number=0x%x\n", n);
+}
+
+uint32_t my_ntohl(uint32_t n) {
+	return n << 24 | n >> 24 | (n & 0x0000ff00) << 8 | (n & 0x00ff0000) >> 8;
 }
 
 void  write_0x12345678() {
