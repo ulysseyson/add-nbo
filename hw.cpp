d97c21dde739c3e93ff058e0018f5b64f8b90b30
ulysseyson <ulysseyson57@gmail.com>
2023-03-14 09:22:48 +0900

    fix fread

--- hw.cpp
+++ hw.cpp
@@ -5,15 +5,17 @@ uint32_t my_ntohl(uint32_t n) {
 	return n << 24 | n >> 24 | (n & 0x0000ff00) << 8 | (n & 0x00ff0000) >> 8;
 }
 
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
 
-    uint32_t n1 = fopen(argv[1], "r");
-    uint32_t n2 = fopen(argv[2], "r");
+    FILE *fp1 = fopen(argv[1], "r");
+    FILE *fp2 = fopen(argv[2], "r");
 
+    uint32_t n1 = fread(fp1, 1, 4);
+    uint32_t n2 = fread(fp2, 1, 4);
+    
     n1 = my_ntohl(n1);
     n2 = my_ntohl(n2);
 
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", n1, n1, n2, n2, n1+n2, n1+n2);
-
     return 0;
 }
