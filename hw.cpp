f61da39ebb7e0001907844d6f69e63a7efc6a145
ulysseyson <ulysseyson57@gmail.com>
2023-03-14 16:01:53 +0900

    final fix

--- hw.cpp
+++ hw.cpp
@@ -1,21 +1,18 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <netinet/in.h>
 
-uint32_t my_ntohl(uint32_t n) {
-	return n << 24 | n >> 24 | (n & 0x0000ff00) << 8 | (n & 0x00ff0000) >> 8;
-}
 
 int main(int argc, char *argv[]){
-
     FILE *fp1 = fopen(argv[1], "r");
     FILE *fp2 = fopen(argv[2], "r");
-
-    uint32_t n1 = fread(fp1, 1, 4);
-    uint32_t n2 = fread(fp2, 1, 4);
+    uint32_t n1 , n2;
+    fread(&n1, 4, 1, fp1);
+    fread(&n2, 4, 1, fp2);
     
-    n1 = my_ntohl(n1);
-    n2 = my_ntohl(n2);
+    uint32_t nn1 = ntohl(n1);
+    uint32_t nn2 = ntohl(n2);
 
-    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", n1, n1, n2, n2, n1+n2, n1+n2);
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", nn1, nn1, nn2, nn2, nn1+nn2, nn1+nn2);
     return 0;
 }
